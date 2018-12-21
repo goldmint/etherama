e6217656468832a1b2419673c5b7322de95fba8f
Igor Kurilo <igor.kurilo31@gmail.com>
2018-12-21 16:16:07 +0200

    translate for faq

--- WebApplication/Client/src/app/components/faq/faq.component.ts
+++ WebApplication/Client/src/app/components/faq/faq.component.ts
@@ -16,7 +16,7 @@ export class FaqComponent implements OnInit, OnDestroy {
   public collapse: any = {};
   public totalCollapses = {
     about: 10,
-    faq: 12
+    faq: 11
   }
   public ngForArray = new Array(this.totalCollapses.faq);
 
